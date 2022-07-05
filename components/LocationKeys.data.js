7ef75c515eb6de9e8cca31281b1b9434bf1ae492
tonkaew131 <nongtonkaew@gmail.com>
2022-07-05 19:58:34 +0700

    Location Keys: Update TH Locales

--- components/LocationKeys.data.js
+++ components/LocationKeys.data.js
@@ -2,7 +2,9 @@ const locationKeys = {
     '2032097': {
         name: 'Ban Bang Rak Yai, Nonthaburi',
         city: 'Ban Bang Rak Yai',
+        city_th: 'บางรักใหญ่',
         province: 'Nonthaburi',
+        province_th: 'นนทบุรี',
         geo_position: {
             lat: 13.883,
             long: 100.438
